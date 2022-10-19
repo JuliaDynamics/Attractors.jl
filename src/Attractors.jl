888b09a95f3ed69f4d4ecc3e0d12520481c54152
Datseris <datseris.george@gmail.com>
2022-10-19 12:37:51 +0100

    add source code from ChaosTools.jl

--- src/Attractors.jl
+++ src/Attractors.jl
@@ -5,4 +5,15 @@ Part of [DynamicalSystems.jl](https://juliadynamics.github.io/DynamicalSystems.j
 """
 module Attractors
 
+
+include("dict_utils.jl")
+include("mapping/attractor_mapping.jl")
+include("basins_of_attraction.jl")
+include("basins_utilities.jl")
+include("fractality_of_basins.jl")
+include("tipping.jl")
+include("sampler.jl")
+include("continuation/basins_fractions_continuation_api.jl")
+
+
 end # module Attractors
