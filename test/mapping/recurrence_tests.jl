e65e13bba86ecac602bfba9c482e401306e08a06
kalelrossi <kalelluizrossi@gmail.com>
2022-10-26 13:17:50 +0200

    change test to Henon map, now sparse and nonsparse give equal results

--- test/mapping/recurrence_tests.jl
+++ test/mapping/recurrence_tests.jl
@@ -5,7 +5,7 @@ using Random
 using Statistics
 
 @testset "Compatibility sparse and nonsparse" begin
-    function test_compatibility_sparse_nonsparse(ds, grid; diffeq, kwargs...)
+    function test_compatibility_sparse_nonsparse(ds, grid; diffeq=NamedTuple(), kwargs...)
         sampler, = statespace_sampler(Random.MersenneTwister(1234);
             min_bounds = minimum.(grid), max_bounds = maximum.(grid)
         )
@@ -22,16 +22,13 @@ using Statistics
             @test approx_atts_sparse == approx_atts_non
         nothing
     end
-    @testset "Lorenz84" begin
-        F = 6.886; G = 1.347; a = 0.255; b = 4.0
-        ds = Systems.lorenz84(; F, G, a, b)
-        u0s = [
-            1 => [2.0, 1, 0], # periodic
-            2 => [-2.0, 1, 0], # chaotic
-            3 => [0, 1.5, 1.0], # fixed point
-        ]
-        diffeq = (alg = Vern9(), reltol = 1e-9, abstol = 1e-9)
-        M = 200; z = 3; xg = yg = zg = range(-z, z; length = M); grid = (xg, yg, zg)
-        test_compatibility_sparse_nonsparse(ds, grid; diffeq)
+
+    @testset "Henon map: discrete & divergence" begin
+        ds = Systems.henon(zeros(2); a = 1.4, b = 0.3)
+        u0 = [0.0, 0.0]
+        xg = yg = range(-2.0, 2.0; length=100)
+        grid = (xg, yg)
+        test_compatibility_sparse_nonsparse(ds, grid)
     end
+
 end
