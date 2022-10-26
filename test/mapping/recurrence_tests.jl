6e435cad556636cb644a7dad27e87453149f27ea
kalelrossi <kalelluizrossi@gmail.com>
2022-10-26 10:51:10 +0200

    use morris lecar from dsbase, add to runtests

--- test/mapping/recurrence_tests.jl
+++ test/mapping/recurrence_tests.jl
@@ -4,20 +4,9 @@ using OrdinaryDiffEq:Vern9
 using Random
 using Statistics
 
-function morris_lecar!(du, u, p, t)
-    V1 = -0.00; V2 = 0.15; V4 = 0.1; VCa = 1 ;  VL = -0.5; VK = -0.7; gCa = 1.2; gK = 2; gL = 0.5; τ = 3;
-    I, V3 = p
-    V, N = u
-    M(x) = 0.5*(1 + tanh((x-V1)/V2))
-    G(x) = 0.5*(1 + tanh((x-V3)/V4))
-    du[1] = -gCa*M(V)*(V - VCa) -gK*N*(V - VK) -gL*(V-VL) + I
-    du[2] = 1/τ*(-N + G(V))
-end
-
 @testset "Density of attractors" begin
     function test_morrislecar(; stop_at_dt, store_once_per_cell)
-        I = 0.115; V3 = 0.1; p = [I, V3];
-        df = ContinuousDynamicalSystem(morris_lecar!, [0.0, 0.0], p)
+        df = Systems.morris_lecar(; I = 0.115)
         diffeq = (reltol = 1e-9,  alg = Vern9())
         xg = yg = range(-1,1,length = 20000)
         mapper = AttractorsViaRecurrences(df, (xg, yg);
