4d8132deb5b621922a2a58b7e8adcc88d9c5f72b
Datseris <datseris.george@gmail.com>
2022-11-05 22:20:29 +0100

    better wording in nearest feature

--- src/mapping/grouping/nearest_feature_config.jl
+++ src/mapping/grouping/nearest_feature_config.jl
@@ -6,8 +6,8 @@ using Neighborhood, Distances
 Initialize a struct that contains instructions on how to group features in
 [`AttractorsViaFeaturizing`](@ref). `GroupViaNearestFeature` accepts a `template`,
 which is a vector of features. Then, generated features from initial conditions in
-[`AttractorsViaFeaturizing`](@ref) are always labelled according to the feature
-from the `tempates` that is closest.
+[`AttractorsViaFeaturizing`](@ref) are labelled according to the feature in
+`templates` that is closest (the label is the index of the closest template).
 
 For optimization, `templates` is expected to be a `Vector{<:SVector}`, leading
 to hyper-optimized nearest neighbor searches. For the same reason, it is recommended
@@ -40,9 +40,9 @@ end
 function group_features(features::Vector{<:AbstractVector}, config::GroupViaNearestFeature)
    # prepare for nearest-neighbors algorithm (kNN with k=1)
    template_tree = searchstructure(KDTree, config.templates, config.metric)
-   labels_nested, dists_to_templates = bulksearch(template_tree, features, NeighborNumber(1))
+   labels_nested, distances_nested = bulksearch(template_tree, features, NeighborNumber(1))
    labels = reduce(vcat, labels_nested) # make it a vector
-   distances = reduce(vcat, dists_to_templates)
+   distances = reduce(vcat, distances_nested)
    # Make label -1 if error strictly bigger than threshold
    labels[distances .> config.max_distance_template] .= -1
    # labels[i] is the label of template nearest to feature i
