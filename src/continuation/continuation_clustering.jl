ecd030a4b02d3793194a0ea56dc1d490b7dafe41
Datseris <datseris.george@gmail.com>
2022-11-17 09:54:30 +0100

    allow code rework for clustering attractors

--- src/continuation/continuation_clustering.jl
+++ src/continuation/continuation_clustering.jl
@@ -77,37 +77,48 @@ function basins_fractions_continuation(
     )
     (; mapper, info_extraction, par_weight) = continuation
     spp, n = samples_per_parameter, length(prange)
-
     features = _get_features_prange(mapper, ics, n, spp, prange, pidx, show_progress)
+    cluster_labels = cluster_all_features(
+        features, mapper.group_config, par_weight;
+        prange, use_mmap = continuation.use_mmap, show_progress,
+        samples_per_parameter
+    )
+    fractions_curves, attractors_info = _label_fractions(cluster_labels, n, spp, features[1], info_extraction)
+    return fractions_curves, attractors_info
+end
 
-    # The distance matrix can get very large. The use of memory map based array is
-    # necessary.
-    if continuation.use_mmap
-        # Create temp file
+# TODO: This whole business makes me feel like we should
+# just make the `mmap` and then `par_weight` parameters part of `GroupViaClustering`,
+# and then the below function is a literaly call to `group_features`...
+# Although it does become a pain in the ass with all this keeping track of the
+# samples per parameter nad the length of the paramter space... GRRRR
+# But I guess we can define a single 5 argument method that calls the 2 argument
+# metod only for the `GroupViaClustering` type.
+function cluster_all_features(
+        features, clust_config, par_weight;
+        prange = nothing, use_mmap = false, show_progress = true,
+        samples_per_parameter = 100,
+    )
+    # The distance matrix can get very large so we allow  memory map based array
+    if use_mmap
         pth, s = mktemp()
         dists = Mmap.mmap(s, Matrix{Float32}, (length(features), length(features)))
     else
         dists = zeros(length(features), length(features))
     end
-
-    progress = ProgressMeter.ProgressUnknown(;
-        desc="Clustering: ", enabled=show_progress
-    )
+    progress = ProgressMeter.ProgressUnknown(desc="Clustering", enabled=show_progress)
     ProgressMeter.next!(progress)
-    _get_dist_matrix!(features, dists, prange, spp, par_weight, mapper)
+    _get_dist_matrix!(features, dists, prange, samples_per_parameter, par_weight, clust_config)
     ProgressMeter.next!(progress)
-    cluster_labels = _cluster_across_parameters(dists, features, mapper)
+    cluster_labels = _cluster_across_parameters(dists, features, clust_config)
     ProgressMeter.finish!(progress)
-
-    fractions_curves, attractors_info = _label_fractions(cluster_labels, n, spp, features[1], info_extraction)
-
-    return fractions_curves, attractors_info
+    return cluster_labels
 end
 
 
 function _get_features_prange(mapper::AttractorsViaFeaturizing, ics, n, spp, prange, pidx, show_progress)
     progress = ProgressMeter.Progress(n;
-        desc="Generating features: ", enabled=show_progress
+        desc="Generating features", enabled=show_progress
     )
     # Extract the first possible feature to initialize the features container
     feature = extract_features(mapper, ics; N = 1)
@@ -123,38 +134,41 @@ function _get_features_prange(mapper::AttractorsViaFeaturizing, ics, n, spp, pra
 end
 
 
-function _get_dist_matrix!(features, dists, prange, spp, par_weight,
-    mapper::AttractorsViaFeaturizing)
+function _get_dist_matrix!(features, dists, prange, spp, par_weight, clust_config)
     # Construct distance matrix
-    metric = mapper.group_config.clust_distance_metric
+    metric = clust_config.clust_distance_metric
     pairwise!(metric, dists, features; symmetric = true)
     # use parameter distance weight (w is the weight for one parameter only)
     # Parameter range is normalized from 0 to 1.
-    par_array = kron(range(0,1,length(prange)), ones(spp))
-    @inbounds for k in eachindex(par_array)
-        for j in eachindex(par_array)
-            dists[k,j] += par_weight*abs(par_array[k] - par_array[j])
+    if par_weight ≠ 0
+        isnothing(prange) && error("`par_weight` isn't 0, but `prange` isn't given!")
+        par_array = kron(range(0,1,length(prange)), ones(spp))
+        @inbounds for k in eachindex(par_array)
+            for j in eachindex(par_array)
+                dists[k,j] += par_weight*abs(par_array[k] - par_array[j])
+            end
         end
     end
 end
 
-function _cluster_across_parameters(dists, features, mapper::AttractorsViaFeaturizing)
-    # Cluster the values accross parameters
-    cc = mapper.group_config
+function _cluster_across_parameters(dists, features, cc::GroupViaClustering)
     ϵ_optimal =  _extract_ϵ_optimal(features, cc)
     cluster_labels = _cluster_distances_into_labels(dists, ϵ_optimal, cc.min_neighbors)
     return cluster_labels
 end
 
 function _label_fractions(cluster_labels, n, spp, feature, info_extraction)
-    # And finally collect/group stuff into their dictionaries
+    # finally we collect/group stuff into their dictionaries
     fractions_curves = Vector{Dict{Int, Float64}}(undef, n)
     dummy_info = info_extraction(feature)
     attractors_info = Vector{Dict{Int, typeof(dummy_info)}}(undef, n)
     for i in 1:n
+        # Here we know which indices correspond to which parameter value
+        # because they are sequentially increased every `spp`
+        # (steps per parameter)
         current_labels = view(cluster_labels, ((i - 1)*spp + 1):i*spp)
         current_ids = unique(current_labels)
-        # getting fractions is easy; use predefined function
+        # getting fractions is easy; use API function that takes in arrays
         fractions_curves[i] = basins_fractions(current_labels, current_ids)
         attractors_info[i] = Dict(id => info_extraction(
             view(current_labels, findall(isequal(id), current_labels)))
