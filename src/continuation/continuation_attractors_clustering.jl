b33128383c9c38d0ab04d0163d3866023f97767a
awage <>
2022-11-17 15:34:22 +0100

    savepoint

--- src/continuation/continuation_attractors_clustering.jl
+++ src/continuation/continuation_attractors_clustering.jl
@@ -39,13 +39,13 @@ function basins_fractions_continuation(
     (; mapper, cluster_in_slice, par_weight, group_config) = continuation
     spp, n = samples_per_parameter, length(prange)
 
-    keys,  pars, vecs, f_curves, att_info = _get_attractors_prange(mapper, ics, n, spp, prange, pidx, show_progress)
+    keys,  pindex, vecs, f_curves, att_info = _get_attractors_prange(mapper, ics, n, spp, prange, pidx, show_progress)
 
-    dists = _get_dist_matrix(vecs, pars, spp, par_weight, cluster_in_slice, group_config)
+    dists = _get_dist_matrix(vecs, pindex, prange, par_weight, cluster_in_slice, group_config)
 
     cluster_labels = _cluster_across_parameters(dists, vecs, group_config)
 
-    _label_fractions!(cluster_labels, n, keys, pars, prange, f_curves, att_info)
+    _label_fractions!(cluster_labels, n, keys, pindex, prange, f_curves, att_info)
 
     return f_curves, att_info
 end
@@ -72,29 +72,29 @@ function _get_attractors_prange(mapper::AttractorsViaRecurrences, ics, n, spp, p
     end
     # collect Datasets
     vec_att = Dataset[]
-    par_array = Float64[]
+    par_index = Int64[]
     key_array = Int64[]
     for (k,att) in enumerate(attractors_info)
         for a in att
             push!(vec_att, a[2])
             push!(key_array, a[1])
-            push!(par_array, prange[k])
+            push!(par_index, k)
         end
     end
-    return key_array, par_array, vec_att, fractions_curves, attractors_info
+    return key_array, par_index, vec_att, fractions_curves, attractors_info
 end
 
 
-function _get_dist_matrix(vec_att, par_array, spp, par_weight, cluster_in_slice, group_config::GroupViaClustering)
+function _get_dist_matrix(vec_att, p_index, p_range, par_weight, cluster_in_slice, group_config::GroupViaClustering)
     metric = group_config.clust_distance_metric
     # Construct distance matrix
     dists = pairwise(metric, vec_att; symmetric = true)
-    par_weight = 1/(par_array[end]-par_array[1])
-    for k in 1:length(par_array)
-        for j in 1:length(par_array)
-            dists[k,j] += par_weight*abs(par_array[k]-par_array[j])
+    par_weight = 1/norm(p_range[end]-p_range[1])
+    for (k,pk) in enumerate(p_index)
+        for (j,pj) in enumerate(p_index)
+            dists[k,j] += par_weight*norm(p_range[pk]-p_range[pj])
             # Add a weight to the dist if we do not want to cluster for the same parameter slice
-            if par_array[k] == par_array[j]
+            if p_range[pk] == p_range[pj]
                 dists[k,j] += cluster_in_slice
             end
         end
@@ -114,11 +114,11 @@ function _cluster_across_parameters(dists, vecs, group_config)
     return cluster_labels
 end
 
-function _label_fractions!(cluster_labels, n, key_array, par_array, prange, fractions_curves, attractors_info)
+function _label_fractions!(cluster_labels, n, key_array, pindex, prange, fractions_curves, attractors_info)
     # And finally collect/group stuff into their dictionaries
     c = 0; next_label = maximum(cluster_labels) + 1
     for i in 1:n
-        l = sum(par_array .== prange[i])
+        l = sum(pindex .== i)
         current_labels = view(cluster_labels, c+1:c+l)
         d = Dict()
         for k in c+1:c+l
--- test/continuation/clustering_attractors_tests.jl
+++ test/continuation/clustering_attractors_tests.jl
@@ -2,9 +2,9 @@ using Test, Attractors
 using Attractors.DynamicalSystemsBase
 using Random
 
-@testset "Henon map" begin
+@testset "Clustering Attractors: Henon map" begin
     ds = Systems.henon(; b = -0.9, a = 1.4)
-    prange = range(0.6, 1.1; length = 20)
+    prange = range(0.6, 1.1; length = 10)
     pidx = 1; spp = 10000
     xg = yg = range(-2,2, length = 1000)
     grid = (xg,yg)
@@ -14,7 +14,7 @@ using Random
     ## CLUSTERING CONTINUATION 
     dts_dis(x,y)= dataset_distance(x,y, Hausdorff())
     gc = GroupViaClustering(;clust_distance_metric = dts_dis, optimal_radius_method = 1., min_neighbors = 1)
-    continuation = ClusteringAttractorsContinuation(mapper, par_weight = 1., group_config = gc)
+    continuation = ClusteringAttractorsContinuation(mapper; par_weight = 1., group_config = gc)
     fs, att = basins_fractions_continuation(
         continuation,  prange, pidx, sampler; 
         show_progress = false, samples_per_parameter = spp)
@@ -32,9 +32,11 @@ using Random
     for k in 1:length(prange)
         v1 = collect(values(fs[k]))
         v2 = collect(values(fs2[k]))
-        for d in  ((sort(v1) .- sort(v2)) .< 0.001)
-            @test d
-        end
+        @show v1, v2
+        # @test length(v1) == length(v2)
+        # for d in  ((sort(v1) .- sort(v2)) .< 0.001)
+        #     @test d
+        # end
     end
 
     # compare number of labels detected
@@ -46,3 +48,48 @@ using Random
     @test sort(unique(k1)) == sort(unique(k2))
 end
 
+# @testset "Clustering Attractors: magnetic pendulum" begin
+    d, α, ω = 0.3, 0.2, 0.5
+    ds = Systems.magnetic_pendulum(; d, α, ω)
+    xg = yg = range(-3, 3, length = 101); grid = (xg, yg)
+    ds = projected_integrator(ds, 1:2, [0.0, 0.0])
+    mapper = AttractorsViaRecurrences(ds, (xg, yg); Δt = 1.0)
+    rr = range(1, 0; length = 21)
+    ps = [[1, 1, γ] for γ in rr]
+    pidx = :γs
+    spp = 100
+
+    # RECURENCE CONTINUATION
+    sampler, = statespace_sampler(Random.MersenneTwister(1234); min_bounds = minimum.(grid), max_bounds = maximum.(grid))
+    continuation = RecurrencesSeedingContinuation(mapper; threshold = Inf)
+    fs2, att2 = basins_fractions_continuation(
+        continuation, ps, pidx, sampler; show_progress = true, samples_per_parameter = spp
+    )
+    ## CLUSTERING CONTINUATION 
+    sampler, = statespace_sampler(Random.MersenneTwister(1234); min_bounds = minimum.(grid), max_bounds = maximum.(grid))
+    dts_dis(x,y)= dataset_distance(x,y, Hausdorff())
+    gc = GroupViaClustering(;clust_distance_metric = dts_dis, optimal_radius_method = .2, min_neighbors = 1)
+    continuation = ClusteringAttractorsContinuation(mapper; par_weight = 1., group_config = gc)
+    fs, att = basins_fractions_continuation(
+        continuation,  ps, pidx, sampler; 
+        show_progress = true, samples_per_parameter = spp)
+    
+     
+    # Compare fractions 
+    for k in 1:length(ps)
+        v1 = collect(values(fs[k]))
+        v2 = collect(values(fs2[k]))
+        @show v1, v2
+        for d in  ((sort(v1) .- sort(v2)) .< 0.001)
+            @test d
+        end
+    end
+
+    # compare number of labels detected
+    k1 = Int[]; k2 = Int[];
+    for k in 1:length(ps)
+        push!(k1, collect(keys(fs[k]))...)
+        push!(k2, collect(keys(fs2[k]))...)
+    end
+    @test sort(unique(k1)) == sort(unique(k2))
+# end
