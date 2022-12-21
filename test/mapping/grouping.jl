e3b2982f0a61f203c2bdf86dd83713af67c6386b
George Datseris <datseris.george@gmail.com>
2022-12-21 12:09:39 +0200

    add mmap tests

--- test/mapping/grouping.jl
+++ test/mapping/grouping.jl
@@ -7,7 +7,7 @@ if DO_EXTENSIVE_TESTS
 using Attractors
 using Statistics, Random, Test
 
-@testset "Artificial clustering" begin
+@testset "Clustering" begin
     ## prepare test (three possible attractors)
     function featurizer(A, t)
         return SVector(maximum(A[:,1]), maximum(A[:,2]))
@@ -22,9 +22,8 @@ using Statistics, Random, Test
     a[end] = [50 5]; correct_labels[end] = -1; correct_labels_infinite_threshold[end] = 3;
     attractors = Dict(1:length(a) .=> Dataset.(a; warn = false));
 
-    ## Unsupervised
     ### silhouettes and real
-    for optimal_radius_method in ["silhouettes", "silhouettes_optim", 5.0]
+    @testset "method=$(optimal_radius_method)" for optimal_radius_method in ["silhouettes", "silhouettes_optim", 5.0]
         for silhouette_statistic in [mean, minimum]
             clusterspecs = GroupViaClustering(; num_attempts_radius=20, silhouette_statistic,
             optimal_radius_method=optimal_radius_method, min_neighbors=2, rescale_features=false)
@@ -34,19 +33,29 @@ using Statistics, Random, Test
     end
 
     ### knee method
-    correct_labels_knee = [1,1,1,1, 2,2,2,1,2,3,3,3,3,1,2,2,2,2,2,3,3,3,3,3,3,1,1,1,1,1] #smaller number of features works even worse
-    Random.seed!(1)
-    a = [attractor_pool[label] + 0.2*rand(Float64, (1,2)) for label in correct_labels_knee]
-    attractors_knee = Dict(1:length(a) .=> Dataset.(a; warn = false));
-    clusterspecs = GroupViaClustering( optimal_radius_method="knee",
-    min_neighbors=4, rescale_features=false)
-    clust_labels = cluster_datasets(featurizer, [], attractors_knee, clusterspecs)
-    # at least check if it finds the same amount of attractors;
-    # note this does not work for any value of `min_neighbors`.
-    @test maximum(clust_labels) == maximum(correct_labels)
+    @testset "method=knee" begin
+        correct_labels_knee = [1,1,1,1, 2,2,2,1,2,3,3,3,3,1,2,2,2,2,2,3,3,3,3,3,3,1,1,1,1,1] #smaller number of features works even worse
+        Random.seed!(1)
+        a = [attractor_pool[label] + 0.2*rand(Float64, (1,2)) for label in correct_labels_knee]
+        attractors_knee = Dict(1:length(a) .=> Dataset.(a; warn = false));
+        clusterspecs = GroupViaClustering( optimal_radius_method="knee",
+        min_neighbors=4, rescale_features=false)
+        clust_labels = cluster_datasets(featurizer, [], attractors_knee, clusterspecs)
+        # at least check if it finds the same amount of attractors;
+        # note this does not work for any value of `min_neighbors`.
+        @test maximum(clust_labels) == maximum(correct_labels)
+    end
+
+    @testset "Mmap" begin
+        clusterspecs = GroupViaClustering(; num_attempts_radius=20, use_mmap=true,
+        min_neighbors=2, rescale_features=false)
+        clust_labels = cluster_datasets(featurizer, [], attractors, clusterspecs)
+        @test clust_labels == correct_labels
+    end
+
 end
 
-@testset "Histogram grouping" begin
+@testset "Histogram" begin
 
 end
 
