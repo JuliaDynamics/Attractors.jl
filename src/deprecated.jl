697db16b60ca16ec2530398c5933f914a23e94cf
Datseris <datseris.george@gmail.com>
2023-02-06 16:32:48 +0000

    delete all deprecated stuff; too hard to mainatain backwards compat

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,15 +1,15 @@
 # This function is deprecated, however it will NEVER be removed.
 # In the future, this call signature will remain here but its source code
 # will simply be replaced by an error message.
-function basins_of_attraction(grid::Tuple, ds::DynamicalSystem;
-        kwargs...
-    )
-
+function basins_of_attraction(grid::Tuple, ds::DynamicalSystem; kwargs...)
     error("""
     The function `basins_of_attraction(grid::Tuple, ds::DynamicalSystem; ...)` has
     been replaced by the more generic
     `basins_of_attraction(mapper::AttractorMapper, grid::Tuple`) which works for
-    any instance of `AttractorMapper`. Please use that method instead.
-    """)
+    any instance of `AttractorMapper`. The `AttractorMapper` itself requires as
+    input an kind of dynamical system the user wants, like a `StroboscopicMap` or
+    `CoupledODEs` or `DeterministicIteratedMap` etc.
 
+    Please use that method instead.
+    """)
 end
