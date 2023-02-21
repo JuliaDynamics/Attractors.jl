46e3f7eceb5ee23abecbf9fd7df4f8a3efba361d
Datseris <datseris.george@gmail.com>
2023-02-21 13:46:59 +0000

    better docstring

--- src/continuation/basins_fractions_continuation_api.jl
+++ src/continuation/basins_fractions_continuation_api.jl
@@ -7,42 +7,36 @@ export continuation
 abstract type AttractorsBasinsContinuation end
 
 """
-   continuation(continuation, prange, pidx, ics; kwargs...)
+   continuation(abc::AttractorsBasinsContinuation, prange, pidx, ics; kwargs...)
 
-Find and continue attractors and the fractions of their basins of attraction
-across a parameter range.
-The given `continuation` contains a reference to a dynamical system,
-as well as how to find its attractors. I.e., it contains an [`AttractorMapper`](@ref).
-Given this `continuation`, the basin fractions (and the attractors for the
-`RecurrencesSeedingContinuation` method) are continued across the parameter range `prange`,
+Find and continue attractors (or feature-based representations of attractors)
+and the fractions of their basins of attraction across a parameter range.
+
+The continuation type `abc` is a subtype of [`AttractorsBasinsContinuation`](@ref)
+and contains an [`AttractorMapper`](@ref). The mapper contains information
+on how to find the attractors and basins of a dynamical system. Additional
+arguments and keyword arguments given when creating `abc` further tune the continuation.
+
+In the `continuation` function, the basin fractions and the attractors (or some representation
+of them in the case of featurizing) are continued across the parameter range `prange`,
 for the parameter of the system with index `pidx`.
 `ics` is as in [`basins_fractions`](@ref), i.e., it is either a function generating
-initial conditions or a dataset containing them.
+initial conditions or a set containing them.
 
 ## Return
 
-1. `fractions_curves <: Vector{Dict{Int, Float64}}`. The fractions of basins of attraction.
+1. `fractions_curves :: Vector{Dict{Int, Float64}}`. The fractions of basins of attraction.
    `fractions_curves[i]` is a dictionary mapping attractor IDs to their basin fraction
    at the `i`-th parameter.
 2. `attractors_info <: Vector{Dict{Int, <:Any}}`. Information about the attractors.
    `attractors_info[i]` is a dictionary mapping attractor ID to information about the
    attractor at the `i`-th parameter.
-   The type of information stored depends on the chosen continuation method.
+   The type of information stored depends on the chosen continuation type.
 
 ## Keyword arguments
 
-* `samples_per_parameter = 100`: Amount of initial conditions sampled at each parameter.
-* `show_progress = true`: Whether to show a progress bar for the process.
-
-## Continuation methods
-
-- [`RecurrencesSeedingContinuation`](@ref). For this sampler, `ics` is optional.
-  If not given, one is created using the `grid` of [`AttractorsViaRecurrences`](@ref):
-  ```
-  sampler, = statespace_sampler(min_bounds = minimum.(grid), max_bounds = maximum.(grid))
-  ```
-- [`GroupAcrossParameterContinuation`](@ref).
-
+- `show_progress = true`: display a progress bar of the computation.
+* `samples_per_parameter = 100`: amount of initial conditions sampled at each parameter.
 """
 function continuation end
 
