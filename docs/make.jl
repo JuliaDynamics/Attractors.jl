d13bb6b648431f8eafdba8cf1f6d1b22c23e4124
Datseris <datseris.george@gmail.com>
2023-02-21 15:23:44 +0000

    use automated doc build

--- docs/make.jl
+++ docs/make.jl
@@ -1,36 +1,8 @@
 cd(@__DIR__)
-using Pkg
-Pkg.activate(@__DIR__)
-CI = get(ENV, "CI", nothing) == "true" || get(ENV, "GITHUB_TOKEN", nothing) !== nothing
-using Documenter
-using DocumenterTools: Themes
-ENV["JULIA_DEBUG"] = "Documenter"
-using CairoMakie
 using Attractors
+using Attractors.DynamicalSystemsBase
 using Attractors.StateSpaceSets
 
-# %% JuliaDynamics theme
-# It includes themeing for the HTML build
-# and themeing for the Makie plotting
-
-using DocumenterTools: Themes
-for file in ("juliadynamics-lightdefs.scss", "juliadynamics-darkdefs.scss", "juliadynamics-style.scss")
-    filepath = joinpath(@__DIR__, file)
-    if !isfile(filepath)
-        download("https://raw.githubusercontent.com/JuliaDynamics/doctheme/master/$file", joinpath(@__DIR__, file))
-    end
-end
-# create the themes
-for w in ("light", "dark")
-    header = read(joinpath(@__DIR__, "juliadynamics-style.scss"), String)
-    theme = read(joinpath(@__DIR__, "juliadynamics-$(w)defs.scss"), String)
-    write(joinpath(@__DIR__, "juliadynamics-$(w).scss"), header*"\n"*theme)
-end
-# compile the themes
-Themes.compile(joinpath(@__DIR__, "juliadynamics-light.scss"), joinpath(@__DIR__, "src/assets/themes/documenter-light.css"))
-Themes.compile(joinpath(@__DIR__, "juliadynamics-dark.scss"), joinpath(@__DIR__, "src/assets/themes/documenter-dark.css"))
-
-# %% Build docs
 PAGES = [
     "index.md",
     "attractors.md",
@@ -39,29 +11,13 @@ PAGES = [
     "examples.md",
 ]
 
-include("style.jl")
-
-makedocs(
-    modules = [Attractors, StateSpaceSets],
-    format = Documenter.HTML(
-        prettyurls = CI,
-        assets = [
-            asset("https://fonts.googleapis.com/css?family=Montserrat|Source+Code+Pro&display=swap", class=:css),
-        ],
-        collapselevel = 3,
-        ),
-    sitename = "Attractors.jl",
-    authors = "George Datseris, Kalel Rossi, Alexandre Wagemakers",
-    pages = PAGES,
-    doctest = CI,
-    draft = false,
-    expandfirst = ["index.md"], #  this is the first script that loads colorscheme
+import Downloads
+Downloads.download(
+    "https://raw.githubusercontent.com/JuliaDynamics/doctheme/master/build_docs_with_style.jl",
+    joinpath(@__DIR__, "build_docs_with_style.jl")
 )
+include("build_docs_with_style.jl")
 
-if CI
-    deploydocs(
-        repo = "github.com/JuliaDynamics/Attractors.jl.git",
-        target = "build",
-        push_preview = true
-    )
-end
+build_docs_with_style(pages, Attractors, DynamicalSystemsBase, StateSpaceSets;
+    expandfirst = ["index.md"],
+)
