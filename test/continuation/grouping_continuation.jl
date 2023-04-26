e87addaf398c51a6cf97d2eb659030fcda7692fa
Datseris <datseris.george@gmail.com>
2023-04-26 15:47:37 +0100

    revert incorrect parweight in example

--- test/continuation/grouping_continuation.jl
+++ test/continuation/grouping_continuation.jl
@@ -36,7 +36,7 @@ using Random
     featurizer(a, t) = a[end]
     clusterspecs = Attractors.GroupViaClustering(optimal_radius_method = "silhouettes", max_used_features = 200)
     mapper = Attractors.AttractorsViaFeaturizing(ds, featurizer, clusterspecs; T = 20, threaded = true)
-    gap = GroupAcrossParameter(mapper; par_weight = 1.0)
+    gap = GroupAcrossParameter(mapper; par_weight = 0.0)
     fractions_curves, attractors_info = continuation(
         gap, rrange, ridx, sampler; show_progress = false
     )
