2ccd793cd80c5416efa2285fb9f1e7d4c03f29f2
StefanVaylBX2023 <91256224+StefanVaylBX2023@users.noreply.github.com>
2023-07-03 11:23:11 -0400

    bbox

--- src/mfs.jl
+++ src/mfs.jl
@@ -1,29 +1,4 @@
 
-""" 
-    check_basin(mapper, X, Y)
-
-Input:
-    mapper: AttractorMapper
-    X: Point 1
-    Y: Point 2
-Output: 
-    true if X and Y are in the same basin of attraction, false otherwise
-
-Description:
-    This function checks if two points are in the same basin of attraction. 
-    It does so by checking if the points are mapped to the same point by the mapper.
-
-"""
-
-function check_basin(mapper::AttractorMapper, X, Y)
-    idx = mapper(X)
-    idy = mapper(Y)
-    if idx == idy
-        return true
-    else
-        return false
-    end
-end
 
 
 """ 
@@ -52,36 +27,33 @@ function random_point_on_sphere(radius, n)
 end
 
 """ 
-    random_pertubation(mapper, X, search_area, dim,  Ntries=10000)
+    random_pertubation(mapper::AttractorMapper, X, search_area, dim,  total_iterations=10000)
 
-Input:
-    mapper: AttractorMapper
-    X: initial point to test
-    search_area: array of two points defining the search area
-    dim: dimension of the system
-    Ntries: number of random pertubations to try, default = 10000
+
+Generate a random pertubation of the initial point "X" within specified "search_area" and checks if it is in the same basin of attraction.
+It does so by generating a random vector of length dim and then adding it to X.
+If the pertubation is not in the same basin of attraction, it calculates the norm of the pertubation and compares it to the best pertubation found so far.
+If the norm is smaller, it updates the best pertubation found so far.
+It repeats this process total_iterations times and returns the best pertubation found.
+
+mapper: AttractorMapper
+X: initial point to be tested
+search_area: array of two points defining the search area
+dim: dimension of the system
+total_iterations: number of random pertubations to try, default = 10000
 
 Output:
     best_shock: vector of the best shock found
     best_dist: norm of the best shock found
-
-Description:
-    This function generates a random pertubation of the initial point "X" within specified "search_area" and checks if it is in the same basin of attraction.
-    It does so by generating a random vector of length dim and then adding it to X.
-    If the pertubation is not in the same basin of attraction, it calculates the norm of the pertubation and compares it to the best pertubation found so far.
-    If the norm is smaller, it updates the best pertubation found so far.
-    It repeats this process Ntries times and returns the best pertubation found.
-
 """
 
-function random_pertubation(mapper::AttractorMapper, X, search_area, dim,  Ntries=10000)
+function random_pertubation(mapper::AttractorMapper, X, search_area, dim, id_x;  total_iterations=10000)
     best_dist = Inf
     best_shock = nothing
-    Xsp = StateSpaceSet([X])
-    for _ in 1:Ntries
+    for _ in 1:total_iterations
         u0 = search_area[1] .+ rand(dim) .* (search_area[2] .- search_area[1])
         shock = X + u0
-        if !(check_basin(mapper, shock, X))
+        if !(id_x == mapper(shock))
             dist = LinearAlgebra.norm(u0)
             if dist < best_dist
                 best_dist = dist
@@ -94,7 +66,7 @@ end
 
 
 """ 
-    mfs_brute_force(mapper, X, best_shock, best_dist, dim,  Ntries=10000)
+    mfs_brute_force(mapper, X, best_shock, best_dist, dim,  total_iterations=10000)
 
 Input:
     mapper: AttractorMapper
@@ -102,7 +74,7 @@ Input:
     best_shock: vector of the best shock found so far
     best_dist: norm of the best shock found so far
     dim: dimension of the system
-    Ntries: number of random pertubations to try, default = 10000
+    total_iterations: number of random pertubations to try, default = 10000
 
 Output:
     best_shock: vector of the best shock found
@@ -112,21 +84,21 @@ Description:
     This function works on the results obtained by random_pertubation(). It starts from the best shock found so far and tries to find a better one 
     by continuously reducing the radius of the sphere on the surface of which it generates random pertubations. If pertubation with the same basin of attraction is found,
     it updates the best shock found so far and reduces the radius of the sphere. 
-    It repeats this process Ntries times and returns the best pertubation found.
+    It repeats this process total_iterations times and returns the best pertubation found.
 """
 
 
-function mfs_brute_force(mapper::AttractorMapper, X, best_shock, best_dist, dim,  Ntries=10000)
+function mfs_brute_force(mapper::AttractorMapper, X, best_shock, best_dist, dim, id_x,  total_iterations=10000)
     
     radius_dif = 100.
 
-    for _ in 1:Ntries
+    for _ in 1:total_iterations
 
         pertubation = random_point_on_sphere((best_dist-best_dist/radius_dif), dim)
         new_shock = X + pertubation
         
-        if !(check_basin(mapper, X, new_shock))
-            best_dist = set_distance(StateSpaceSet([X]), StateSpaceSet([new_shock]), Centroid())
+        if !(id_x == mapper(new_shock))
+            best_dist = LinearAlgebra.norm(pertubation)
             best_shock = pertubation
         end
     end
@@ -135,15 +107,31 @@ function mfs_brute_force(mapper::AttractorMapper, X, best_shock, best_dist, dim,
 end
 
 
+
+
+function mfs_objective(perturbation, X, idX, mapper::AttractorMapper, penalty=1000.0) 
+
+    dist = LinearAlgebra.norm(perturbation)
+    if dist == 0
+        return penalty
+    end
+    if mapper(X + perturbation) == idX
+        return dist + penalty
+    else
+        return dist
+    end
+end
+
+
 """
-    mfs(mapper, search_area, X, dim, n_iterations = 10000)
+    minimal_fatal_shock(mapper, search_area, X, dim, total_iterations = 10000)
 
 Input:
     mapper: AttractorMapper
     search_area: array of two points defining the search area
     X: initial point to test
     dim: dimension of the system
-    n_iterations: number of iterations to run each step of the algorithm, default = 10000
+    total_iterations: number of iterations to run each step of the algorithm, default = 10000
 
 Output:
     best_shock: vector of the best shock found
@@ -155,9 +143,35 @@ Description:
 """
 
 
-function mfs(mapper::AttractorMapper, search_area, X, dim, n_iterations = 10000)
-    best_shock, best_dist = random_pertubation(mapper, X, search_area, dim,  n_iterations)
-    best_shock, best_dist = mfs_brute_force(mapper, X, best_shock, best_dist, dim, n_iterations)
+function minimal_fatal_shock(mapper::AttractorMapper, X, search_area; total_iterations = 10000, algorithm = :random)
+
+
+    id_x = mapper(X)
+    dim = mapper.ds
+
+    function objective_function(perturbation)
+        return mfs_objective(perturbation, X, id_x, mapper)
+    end
+
+    if algorithm == random 
+        best_shock, best_dist = random_pertubation(mapper, X, search_area, dim, id_x;  total_iterations)
+        best_shock, best_dist = mfs_brute_force(mapper, X, best_shock, best_dist, dim, id_x,  total_iterations)
+    elseif algorithm == bboxopt
+        result = bboptimize(objective_function; SearchRange = [search_area for _ in dim], NumDimensions = dim)
+        best_shock = best_candidate(result)
+        best_dist = LinearAlgebra.norm(best_shock)
+    elseif algorithm == combined
+        best_shock, best_dist = random_pertubation(mapper, X, search_area, dim, id_x;  total_iterations)
+        best_shock, best_dist = mfs_brute_force(mapper, X, best_shock, best_dist, dim, id_x,  total_iterations)
+        result = bboptimize(objective_function, best_shock; SearchRange = [search_area for _ in dim], NumDimensions = dim)
+        best_shock = best_candidate(result)
+        best_dist = LinearAlgebra.norm(best_shock)
+    else
+        error("Algorithm not recognized")
+    end
+
     return best_shock, best_dist
 end
+
+
 
