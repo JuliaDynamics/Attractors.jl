f704bda321cf55f5d6b1130afbd6f6610cd1dbf6
kalelrossi <kalelluizrossi@gmail.com>
2023-10-02 18:23:37 +0200

    improve doc with more info and some tips

--- src/mapping/grouping/pairwise_comparison.jl
+++ src/mapping/grouping/pairwise_comparison.jl
@@ -1,13 +1,13 @@
 export GroupViaPairwiseComparison
 
-
 """
     GroupViaPairwiseComparison(; distance_threshold::Real, kwargs...)
 
 Initialize a struct that contains instructions on how to group features in
 [`AttractorsViaFeaturizing`](@ref). `GroupViaPairwiseComparison` groups features and
-identifies clusters by considering the pairwise distance (using `distance_metric`) between
-features. 
+identifies clusters by considering the pairwise distance between features. It can be used
+as an alternative to the clustering method in `GroupViaClustering`, having the
+advantage that it is simpler, typically faster and uses less memory. 
 
 ## Keyword arguments
     
@@ -22,16 +22,28 @@ features.
   separately into the range `[0,1]`. This typically leads to more accurate clustering.
 
 ## Description
-This algorithm assumes that the the features well-separated into distinct clouds, with the
+This algorithm assumes that the features are well-separated into distinct clouds, with the
 maximum radius of the cloud controlled by `distance_threshold`. Since the systems are
 deterministic, this is achievable with a good-enough `featurizer` function, by removing
 transients, and running the trajectories for sufficiently long. It then considers that
 features belong to the same attractor when their pairwise distance, computed using
-`distance_metric`, is smaller or equal than `distance_threshold`, and belong to different
-attractors when the distance is bigger than the threshold. Attractors correspond to each
+`distance_metric`, is smaller than or equal to `distance_threshold`, and that they belong
+to different attractors when the distance is bigger. Attractors correspond to each
 grouping of similar features. In this way, the key parameter `distance_threshold` is
-simply the amount of variations permissible in the features. If they are well-chosen, the
-value can be relatively small and does not need to be fine tuned.
+basically the amount of variation permissible in the features. If they are well-chosen,
+the value can be relatively small and does not need to be fine tuned. 
+
+The `distance_threshold` should achieve a balance: one one hand, it should be large enough
+to account for variations in the features from the same attractor - if it's not large
+enough, the algorithm will find duplicate attractors. On the other hand, it should be
+small enough to not group together features from distinct attractors. This requires some
+knowledge of how spread the features are. If it's too big, the algorithm will miss some
+attractors, as it groups 2+ distinct attractors together. Therefore, as a rule of thumb,
+one can repeat the procedure a few times, starting with a relatively large value and
+reducing it until no more attractors are found and no duplicates appear.
+
+The method uses relatively little memory, as it only stores vectors whose size is on order
+of the number of attractors of the system. 
 """
 struct GroupViaPairwiseComparison{R<:Real, M} <: GroupingConfig
     distance_threshold::R
