490aed5823fd2c89028db50220db4563df202234
George Datseris <datseris.george@gmail.com>
2023-10-04 11:30:30 +0100

    Update src/mapping/recurrences/grids.jl
    Co-authored-by: Alexandre Wagemakers <alexandre.wagemakers@gmail.com>

--- src/mapping/recurrences/grids.jl
+++ src/mapping/recurrences/grids.jl
@@ -33,10 +33,10 @@ end
     SubdivisionBasedGrid(grid::NTuple{D, <:AbstractRange}, lvl_array::Array{Int, D})
 
 Given a coarse `grid` tesselating the state space, construct a `SubdivisionBasedGrid`
-based on the given level array `lvl_array`.
-The level array is has non-negative integer values.
-Value 0 means that the corresponding cell of the coarse `grid` is not subdivided
-any further. Value `n > 0` means that the corresponding cell will be subdivided
+based on the given level array `lvl_array` that should have the same dimension as `grid`. 
+The level array has non-negative integer values, with 0 meaning that the 
+corresponding cell of the coarse `grid` should not be subdivided any further. 
+Value `n > 0` means that the corresponding cell will be subdivided
 in total `2^n` times (along each dimension), resulting in finer cells
 within the original coarse cell.
 """
