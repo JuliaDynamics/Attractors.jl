ad7a3af5e602a6fbfb82cbecce86621a139d635d
reykboerner <reyk.boerner@reading.ac.uk>
2023-10-05 18:47:55 +0100

    worked on requested changes

--- src/boundaries/edgetracking.jl
+++ src/boundaries/edgetracking.jl
@@ -17,7 +17,6 @@ different basins of attraction. The system's `attractors` are specified as a dic
 * `dt = 0.01`: integration time step
 * `ϵ_mapper = 0.1`: `ϵ` parameter in [`AttractorsViaProximity`](@ref)
 * `verbose = false`: if true, prints info while running
-* `output_level = 2`: controls what to return (see below)
 * `kwargs...`: additional keyword arguments to be passed to `AttractorsViaProximity`
 
 ## Description
@@ -40,12 +39,6 @@ parallel integration are then used as new states `u1` and `u2` for a new bisecti
 so on, until a stopping criterion is fulfilled.
 
 ## Output 
-Output can be controlled via the `output_level` argument.
-* `output_level = 0`: returns only the final state on the edge
-* `output_level = 1`: returns the track along the edge as a vector of states
-* `output_level = 2`: returns `[track, bisect1, bisect2]`, where `track` is the track along  
-  the edge and `bisect1`, `bisect2` are vectors containing the bisection points on either  
-  side of the basin boundary
 
 !!! warning
     May behave erroneously when the DiffEq solver of `ds` is set to `alg = SimpleATsit5()`,
@@ -56,24 +49,24 @@ Output can be controlled via the `output_level` argument.
 [^2]: [Schneider et al. (2008)](https://journals.aps.org/pre/pdf/10.1103/PhysRevE.78.037301?casa_token=mLQmTv_cBGUAAAAA%3AVKnQs290sq1MNm-5k8hW7nJeLtVX54I7l-SEGol_HUPSCwziPi-EGDE8ucrDiVMIXZGUbzzam8benFw)
 [^3]: [Luarini and Bodai (2017)](https://iopscience.iop.org/article/10.1088/1361-6544/aa6b11)
 """
-function edgetracking(ds::DynamicalSystem, u1, u2, attractors::Dict;
+function edgetracking(ds::DynamicalSystem, attractors::Dict;
     bisect_thresh=1e-7,
     diverge_thresh=1e-6,
+    u1=attractors[1],
+    u2=attractors[2],
     maxiter=100,
     abstol=0.0,
     tmax=Inf,
     dt=0.01,
     ϵ_mapper=0.1,
-    output_level=2,
-    verbose=false,
+    #verbose=false,
     kwargs...)
     
     pds = ParallelDynamicalSystem(ds, [u1, u2])
     mapper = AttractorsViaProximity(ds, attractors, ϵ_mapper; kwargs...)
     
     edgetracking(pds, mapper;
-        bisect_thresh=bisect_thresh, diverge_thresh=diverge_thresh, maxiter=maxiter, abstol=abstol, dt=dt, tmax=tmax,
-        output_level=output_level, verbose=verbose)
+        bisect_thresh, diverge_thresh, maxiter, abstol, dt, tmax)
 end;
 
 """
@@ -87,22 +80,16 @@ function edgetracking(pds::ParallelDynamicalSystem, mapper::AttractorMapper;
     abstol=0.0,
     dt=0.01,
     tmax=Inf,
-    output_level=2,
     verbose=false)
-    
-    verbose && println("=== Starting edge tracking algorithm ===")
-    
-    u1, u2 = bisect_to_edge(pds, mapper; eps=bisect_thresh)
+        
+    u1, u2 = bisect_to_edge(pds, mapper; bisect_thresh)
     edgestate = (u1 + u2)/2
     
-    if output_level>0
-        bisect_pts1 = [u1]
-        bisect_pts2 = [u2]
-        edge = [edgestate]
-    end
-    verbose && println("... Iteration 1: Edge at $(edgestate)")
+    bisect_pts1 = [u1]
+    bisect_pts2 = [u2]
+    edge = [edgestate]
 
-    correction = abstol + 1
+    correction = Inf
     counter = 1
     # edge track iteration loop
     while (correction > abstol) & (maxiter > counter)
@@ -115,42 +102,35 @@ function edgetracking(pds::ParallelDynamicalSystem, mapper::AttractorMapper;
             step!(pds, dt)
             distance = dist(current_state(pds, 1) - current_state(pds, 2))
             T += dt
-            if output_level>1
-                push!(edge, (current_state(pds, 1) + current_state(pds, 2))/2)
-            end
+            push!(edge, (current_state(pds, 1) + current_state(pds, 2))/2)
         end
-        u1, u2 = bisect_to_edge(pds, mapper; eps=bisect_thresh)
+        u1, u2 = bisect_to_edge(pds, mapper; bisect_thresh)
         edgestate = (u1 + u2)/2
         correction = dist(edgestate - state)
         counter += 1
 
-        if output_level>0
-            push!(bisect_pts1, u1)
-            push!(bisect_pts2, u2)
-            push!(edge, edgestate)
-        end
+        push!(bisect_pts1, u1)
+        push!(bisect_pts2, u2)
+        push!(edge, edgestate)
         
-        verbose && println("... Iteration $(counter): Edge at $(edgestate)")
         (counter == maxiter) && @warn("Reached maximum number of $(maxiter) iterations;
         did not converge.")
     end
 
     (counter < maxiter) && println("Edge-tracking converged after $(counter) iterations.")
 
-    (output_level == 0) && return edgestate
-    (output_level == 1) && return edge
-    (output_level == 2) && return [edge, bisect_pts1, bisect_pts2]
+    return edge, bisect_pts1, bisect_pts2
 end;
 
 """
-    bisect_to_edge(pds::ParallelDynamicalSystem, mapper::AttractorMapper; eps=1e-9)
+    bisect_to_edge(pds::ParallelDynamicalSystem, mapper::AttractorMapper; bisect_thresh=1e-9)
 Finds the basin boundary between two states `u1, u2 = current_states(pds)` by bisecting along a
 straight line in phase space. The states `u1` and `u2` must belong to different basins.
 Returns two new states located on either side of the basin boundary at a maximum 
-(Euclidean) distance of `eps` between each other.
+(Euclidean) distance of `bisect_thresh` between each other.
 
 ## Keyword arguments
-* `eps = 1e-9`: The maximum (Euclidean) distance between the two returned states.
+* `bisect_thresh = 1e-9`: The maximum (Euclidean) distance between the two returned states.
 
 ## Description
 `pds` is a `ParallelDynamicalSystem` with two states. The `mapper` must be an `AttractorMapper`
@@ -161,7 +141,7 @@ times, the method will find one of these intersection points. If more than two a
 exist, one of the two returned states may belong to a different basin than the initial
 conditions `u1` and `u2`. A warning is raised if the bisection involves a third basin.
 """
-function bisect_to_edge(pds::ParallelDynamicalSystem, mapper::AttractorMapper; eps=1e-9)
+function bisect_to_edge(pds::ParallelDynamicalSystem, mapper::AttractorMapper; bisect_thresh=1e-9)
     u1, u2 = current_states(pds)
     idx1, idx2 = mapper(u1), mapper(u2)
     
@@ -170,12 +150,12 @@ function bisect_to_edge(pds::ParallelDynamicalSystem, mapper::AttractorMapper; e
     end
     
     distance = dist(u1-u2)
-    while distance > eps
+    while distance > bisect_thresh
         u_new = (u1 + u2)/2
         idx_new = mapper(u_new)
         # slightly shift u_new if it lands directly on the boundary
         if idx_new == -1
-            u_new += eps*(u_1 - u_2)
+            u_new += bisect_thresh*(u_1 - u_2)
         end
         # update u1 or u2
         if idx_new == idx1
@@ -188,7 +168,11 @@ function bisect_to_edge(pds::ParallelDynamicalSystem, mapper::AttractorMapper; e
         end    
         distance = dist(u1-u2)
     end
-    [u1, u2]
+    u1, u2
 end;
 
 dist(x) = sqrt(sum(abs, x.^2));
+
+function edgestate(edge::StateSpaceSet; keep_last=1)
+    
+end
