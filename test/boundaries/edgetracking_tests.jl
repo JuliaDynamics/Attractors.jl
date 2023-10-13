0594e7ed1c2a9be6007af29b8f6b863f66811a5e
reykboerner <reyk.boerner@reading.ac.uk>
2023-10-13 16:10:47 +0100

    added cubic map test

--- test/boundaries/edgetracking_tests.jl
+++ test/boundaries/edgetracking_tests.jl
@@ -2,6 +2,14 @@ using Attractors
 using Test
 using OrdinaryDiffEq
 
+@testset "Saddle point of cubic map" begin
+    cubicmap(u, p, n) = SVector{1}(p[1]*u[1] - u[1]^3)    
+    ds = DeterministicIteratedMap(cubicmap, [1.0], [2.0])
+    attrs = Dict(1 => StateSpaceSet([1.0]), 2 => StateSpaceSet([-1.0]))
+    saddle = edgetracking(ds, attrs; Δt=1, abstol=1e-8)[1][end]
+    @test saddle[1] <= 1e-8
+end
+
 @testset "Saddle point of FitzHugh-Nagumo system" begin
     fhn(u, p, t) = SVector{2}([10*(u[1] - u[1]^3 - u[2]), -3*u[2] + u[1]])
     ds = CoupledODEs(fhn, ones(2), diffeq=(;alg = Vern9()))
