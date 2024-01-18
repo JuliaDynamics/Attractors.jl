be040800f7ff63338f4ba2d2af5ef1664c677acf
Datseris <datseris.george@gmail.com>
2024-01-18 14:10:38 +0000

    ensure 1st paper code is runnable

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -2,7 +2,7 @@
 # In the future, this call signature will remain here but its source code
 # will simply be replaced by an error message.
 function basins_of_attraction(grid::Tuple, ds::DynamicalSystem; kwargs...)
-    error("""
+    @warn("""
     The function `basins_of_attraction(grid::Tuple, ds::DynamicalSystem; ...)` has
     been replaced by the more generic
     `basins_of_attraction(mapper::AttractorMapper, grid::Tuple`) which works for
@@ -10,8 +10,21 @@ function basins_of_attraction(grid::Tuple, ds::DynamicalSystem; kwargs...)
     input an kind of dynamical system the user wants, like a `StroboscopicMap` or
     `CoupledODEs` or `DeterministicIteratedMap` etc.
 
-    Please use that method instead.
+    For now, we do the following for you:
+    ```
+    mapper = AttractorsViaRecurrences(ds, grid; sparse = false)
+    basins_of_attraction(mapper)
+    ```
+    and we are completely ignoring any keywords you provided (which could be about the
+    differential equation solve, or the metaparameters of the recurrences algorithm).
+
+    We strongly recommend that you study the documentation of Attractors.jl
+    and update your code. The only reason we provide this backwards compatibility
+    is because our first paper "Effortless estimation of basins of attraction"
+    uses this function signature in the script in the paper (which we can't change anymore).
     """)
+    mapper = AttractorsViaRecurrences(ds, grid; sparse = false)
+    return basins_of_attraction(mapper)
 end
 
 @deprecate RecurrencesSeededContinuation RecurrencesFindAndMatch
