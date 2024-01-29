7fa97254336b1b5fe54aff8e8e76962a28873729
awage <>
2024-01-29 19:31:25 +0100

    small test for iterates_of_basins

--- test/mapping/basins_of_attraction.jl
+++ test/mapping/basins_of_attraction.jl
@@ -22,10 +22,23 @@ ds = DeterministicIteratedMap(dumb_map, [0., 0.], [r])
 xg = yg = range(-1.5, 2.5; length=3)
 grid = (xg, yg)
 attrs = Dict(1 => StateSpaceSet([SVector(r, r)]), 2 => StateSpaceSet([SVector(-r, -r)]))
-mapper = AttractorsViaProximity(ds, attrs)
+mapper = AttractorsViaProximity(ds, attrs; Ttr = 0)
 
 basins, atts = basins_of_attraction(mapper, grid; show_progress=false)
 
 @test basins[1, :] == fill(2, 3)
 @test basins[2, :] == fill(1, 3)
 @test basins[3, :] == fill(1, 3)
+
+
+
+basins, atts, iterations = iterates_of_basins_of_attraction(mapper, grid; show_progress=false)
+
+@test basins[1, :] == fill(2, 3)
+@test basins[2, :] == fill(1, 3)
+@test basins[3, :] == fill(1, 3)
+@test length(atts) == 2
+@test iterations == fill(1,3,3) 
+
+
+
