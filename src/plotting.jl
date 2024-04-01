606c379aea83488e28c6bdb33aa82f56987770f4
Datseris <datseris.george@gmail.com>
2024-04-01 11:08:56 +0100

    fix shaded docstring

--- src/plotting.jl
+++ src/plotting.jl
@@ -19,18 +19,19 @@ export heatmap_basins_attractors, heatmap_basins_attractors!
     shaded_basins_heatmap(grid, basins, attractors, iterations; kwargs...)
 
 Plot a heatmap of found (2-dimensional) `basins` of attraction and corresponding
-`attractors`. An matrix `iterations` with the same size of `basins` must be provided
-to shade the color according to the value of this matrix. An small value corresponds
+`attractors`. A matrix `iterations` with the same size of `basins` must be provided
+to shade the color according to the value of this matrix. A small value corresponds
 to a light color and a large value to a darker tone. This is useful to represent
-the number of iterations taken for each initial conditions. See also
-[`iterations_to_converge`]@ref to store this iteration number.
+the number of iterations taken for each initial condition to converge. See also
+[`iterations_to_converge`](@ref) to store this iteration number.
 
 ## Keyword arguments
+
 - `show_attractors = true`: shows the attractor on plot
 - `maxit = maximum(iterations)`: clip the values of `iterations` to
 the value `maxit`. Useful when there are some very long iterations and keep the
 range constrained to a given interval.
-- All the [common plotting keywords](@ref).
+- All the [common plotting keywords](@ref common_plot_kwargs).
 """
 function shaded_basins_heatmap end
 function shaded_basins_heatmap! end
