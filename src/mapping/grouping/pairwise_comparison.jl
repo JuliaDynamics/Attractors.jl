b60ce9da59870bef5ffb794717c780eb70cc96fd
Datseris <datseris.george@gmail.com>
2024-09-18 10:58:55 +0100

    simplify and correct src code for `GroupViaPairwiseComparison`

--- src/mapping/grouping/pairwise_comparison.jl
+++ src/mapping/grouping/pairwise_comparison.jl
@@ -1,7 +1,8 @@
 export GroupViaPairwiseComparison
 
 """
-    GroupViaPairwiseComparison(; threshold::Real, kwargs...)
+    GroupViaPairwiseComparison <: GroupingConfig
+    GroupViaPairwiseComparison(; threshold::Real, metric...)
 
 Initialize a struct that contains instructions on how to group features in
 [`AttractorsViaFeaturizing`](@ref). `GroupViaPairwiseComparison` groups features and
@@ -11,14 +12,17 @@ advantage that it is simpler, typically faster and uses less memory.
 
 ## Keyword arguments
 
-* `threshold` (mandatory): A real number defining the maximum distance two features can be to
+* `threshold = 0.1`: A real number defining the maximum distance two features can have to
   be considered in the same cluster - above the threshold, features are different. This
   value simply needs to be large enough to differentiate clusters.
+  A good value for `threshold` depends on the feature variability within a cluster
+  the chosen metric, and whether features are rescaled. See description below for more.
 * `metric = Euclidean()`: A function `metric(a, b)` that returns the distance between two
   features `a` and `b`, outputs of `featurizer`. Any `Metric` from Distances.jl can be used
   here.
 * `rescale_features = true`: if true, rescale each dimension of the extracted features
-  separately into the range `[0,1]`. This typically leads to more accurate grouping.
+  separately into the range `[0, 1]`. This typically leads to more accurate grouping
+  for the default `metric`.
 
 ## Description
 
@@ -53,64 +57,49 @@ struct GroupViaPairwiseComparison{R<:Real, M} <: GroupingConfig
 end
 
 function GroupViaPairwiseComparison(;
-        threshold, #impossible to set a good default value, depends on the features
-        metric=Euclidean(), rescale_features=false,
+        threshold = 0.1, #impossible to set a good default value, depends on the features
+        metric=Euclidean(), rescale_features=true,
     )
-    return GroupViaPairwiseComparison(
-        threshold,
-        metric, rescale_features,
-    )
+    return GroupViaPairwiseComparison(threshold, metric, rescale_features)
 end
 
-function group_features(
-    features, config::GroupViaPairwiseComparison;
-    kwargs...
-)
+function group_features(features, config::GroupViaPairwiseComparison)
     if config.rescale_features
         features = _rescale_to_01(features)
     end
-
-    labels = _cluster_features_into_labels(features, config, config.threshold; kwargs...)
+    labels = _cluster_features_into_labels(features, config)
     return labels
 end
 
-# TODO: add support for par_weight,plength and spp in the computation of the distance metric?
-function _cluster_features_into_labels(features, config::GroupViaPairwiseComparison, threshold::Real; kwargs...)
-    labels_features = Vector{Int64}(undef, length(features)) #labels of all features
+function _cluster_features_into_labels(features, config::GroupViaPairwiseComparison)
+    labels = ones(length(features))
     metric = config.metric
+    threshold = config.threshold
 
-    # Assign feature 1 as a new attractor
-    labels_features[1] = 1
+    # Assign feature 1 as a new cluster center
+    labels[1] = 1
     cluster_idxs = [1] # idxs of the features that define each cluster
     cluster_labels = [1] # labels for the clusters, going from 1 : num_clusters
     next_cluster_label = 2
 
     for idx_feature = 2:length(features)
         feature = features[idx_feature]
-        dist_to_clusters = _distance_dict(feature, features, cluster_idxs, cluster_labels, metric; kwargs...)
+        # because here the cluster labels are always the positive integers,
+        # we don't need a dictionary. `Vector` keys are the IDs we need.
+        dist_to_clusters = [metric(feature, features[cluster_idx]) for cluster_idx in cluster_idxs]
         min_dist, closest_cluster_label = findmin(dist_to_clusters)
 
-        if min_dist > threshold #bigger than threshold => new attractor
+        if min_dist > threshold # bigger than threshold: new attractor
+            # assign feature as new cluster center
             feature_label = next_cluster_label
             push!(cluster_idxs, idx_feature)
             push!(cluster_labels, next_cluster_label)
-            # @info "New attractor $next_cluster_label, min dist was $min_dist > $threshold" #TODO: allow this when debugging verbose mode on!
             next_cluster_label += 1
-        else #smaller than threshold => assign to closest cluster
+        else # smaller than threshold: assign to closest cluster
             feature_label = closest_cluster_label
         end
 
-        labels_features[idx_feature] = feature_label
+        labels[idx_feature] = feature_label
     end
-    return labels_features
-end
-
-function _distance_dict(feature, features, cluster_idxs, cluster_labels, metric; kwargs...)
-    if metric isa Metric
-        dist_to_clusters = Dict(cluster_label => evaluate(metric, feature, features[cluster_idxs[idx_cluster]]) for (idx_cluster, cluster_label) in enumerate(cluster_labels))
-    else
-        dist_to_clusters = Dict(cluster_label => metric(feature, features[cluster_idxs[idx_cluster]]) for (idx_cluster, cluster_label) in enumerate(cluster_labels))
-    end
-
-    return dist_to_clusters
+    return labels
 end
