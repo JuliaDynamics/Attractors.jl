a944ae06d7ab36a1e93f8b1beb76b8d18bebfdee
Datseris <datseris.george@gmail.com>
2025-04-15 21:07:59 +0100

    make edgetracking give Ttr = 100 by default

--- src/boundaries/edgetracking.jl
+++ src/boundaries/edgetracking.jl
@@ -52,6 +52,9 @@ two initial states must belong to different basins of attraction.
 * `show_progress = true`: if true, shows progress bar and information while running.
 * `verbose = true`: if false, silences print output and warnings while running.
 * `kw...`: additional keyword arguments to be passed to [`AttractorsViaProximity`](@ref).
+  Because internally a proximity mapper is used, and because we want to converge to
+  the attractors as fast as possible, `Ttr = 100` is given by default. `Δt` is also
+  propagated to the proximity mapper.
 
 ## Description
 
@@ -116,7 +119,7 @@ function edgetracking(ds::DynamicalSystem, attractors::Dict;
     kwargs...)
 
     pds = ParallelDynamicalSystem(ds, [u1, u2])
-    mapper = AttractorsViaProximity(ds, attractors; ε = ϵ_mapper, Δt, kwargs...)
+    mapper = AttractorsViaProximity(ds, attractors; ε = ϵ_mapper, Δt, Ttr = 100, kwargs...)
 
     edgetracking(pds, mapper;
         bisect_thresh, diverge_thresh, maxiter, abstol, T_transient, Δt, tmax,
