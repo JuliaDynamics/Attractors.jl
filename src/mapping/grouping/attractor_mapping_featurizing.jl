ec8a4931234d79328d9b04673c8ebaeeb4d4fd05
Datseris <datseris.george@gmail.com>
2026-05-27 22:35:03 +0100

    better docstring

--- src/mapping/grouping/attractor_mapping_featurizing.jl
+++ src/mapping/grouping/attractor_mapping_featurizing.jl
@@ -139,10 +139,10 @@ end
 import ProgressMeter
 
 """
-    extract_features(mapper, ics; N = 1000, show_progress = true)
+    extract_features(mapper::AttractorsViaFeaturizing, ics; N = 1000, show_progress = true)
 
 Return a vector of the features of each initial condition in `ics` (as in
-[`basins_fractions`](@ref)), using the configuration of `mapper::AttractorsViaFeaturizing`.
+[`basins_fractions`](@ref)), using the configuration of `mapper`.
 Keyword `N` is ignored if `ics isa StateSpaceSet`.
 """
 function extract_features(
