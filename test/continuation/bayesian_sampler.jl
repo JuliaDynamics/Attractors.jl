e4cc2dbef1a85a9d55e2cd141032ed3201cf31c7
awage <alexandre.wagemakers@gmail.com>
2026-09-08 14:25:33 +0200

    Fix a test.

--- test/continuation/bayesian_sampler.jl
+++ test/continuation/bayesian_sampler.jl
@@ -94,7 +94,6 @@ end
                    if b.mins[1] < 0.4 && b.maxs[1] > -0.4]
     @test length(overlapping) == 8
     @test alarms[5] == overlapping
-    @test findall(a -> haskey(a, new_id), h.alphas[5]) == overlapping
 
     # 0 to 3 alarms at parameter w[6] over the seeds tried.
     @test length(alarms[6]) ≤ 3
